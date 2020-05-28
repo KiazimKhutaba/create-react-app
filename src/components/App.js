dfe8007608b51ad769b3ceeb1ac59d9c37cdd6ac
Kiazim Khutaba <kiazim.khutaba@yandex.com>
2020-05-28 14:53:38 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+
+const App = () => {
+    return (
+        <div className="container">
+            This is React App!
+        </div>
+    );
+};
+
+export default App;
